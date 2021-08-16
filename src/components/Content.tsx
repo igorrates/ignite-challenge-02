d83f92878d58ac49ab6e9a5fcdc37731b12163c8
Igor Rates <igor@corebridge.net>
2021-08-15 23:28:46 -0300

    Finished challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
-  // Complete aqui
+import { useState, useEffect } from "react";
+import { GenreResponseProps } from "../models/GenreResponseProps";
+import { MovieProps } from "../models/MovieProps";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+export function Content(props: {genreId: number}) {
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.genreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${props.genreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [props.genreId]);
+
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
---
+++ src/models/SideBarProps.ts
@@ -0,0 +1,4 @@
+export interface SideBarProps {
+    selectedGenreId: number;
+    setSelectedGenreId: (id: number) => void;
+  }
